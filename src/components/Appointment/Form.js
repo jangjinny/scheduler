d4a731ddf11c1c1116e8c73420c0f7fbf12f40a5
Jinny Jang <jangjinny21@gmail.com>
2021-02-16 20:10:31 -0800

    add form component

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewList";
+
+// Form has 5 props: name(string), interviewers(array), interviewer(number), onSave(func), onCancel(func)
+
+export default function Form(props) {
+
+  const [name, setName ] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  console.log("These are my props:", props)
+  console.log("This is my name", name)
+  console.log("This is my interviwer", interviewer);
+
+  const reset = () => {
+      setName("");
+      setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+
+          <input
+            className="appointment__create-input text--semi-bold"
+            value={name}
+            type="text"
+            placeholder="Enter Student Name"
+            onChange={(event) => setName(event.target.value)}
+            /*
+              This must be a controlled component
+            */
+          />
+
+        </form>
+
+        <InterviewerList 
+          interviewers={props.interviewers} 
+          interviewer={interviewer}
+          setInterviewer={setInterviewer}
+        />
+
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel} >Cancel</Button>
+          <Button confirm onClick={props.onSave(name, interviewer)}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+
+};
--- src/components/InterviewList.js
+++ src/components/InterviewList.js
@@ -2,6 +2,8 @@ import React from "react";
 import InterviewListItem from "components/InterviewListItem";
 import "components/InterviewList.scss";
 
+//Interviewer List has 3 props: interviewers(array), interviewer(number), setInterviewer(func)
+
 export default function InterviewList(props) {
 
   const mapInterviewers = props.interviewers.map(interviewer => {
