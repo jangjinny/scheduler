18dc26a6ca0711d96076ba0fca53710fcb63c313
Jinny Jang <jangjinny21@gmail.com>
2021-02-26 01:32:32 -0800

    move formatSpots function outside the component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,26 +2,26 @@ import React from "react";
 import "components/DayListItem.scss";
 import classNames from "classnames";
 
+const formatSpots = (spots) => {
+  if (!spots) {
+    return "no spots remaining";
+  } else if (spots === 1) {
+    return "1 spot remaining";
+  } else {
+    return spots + " spots remaining";
+  }
+}
+
 export default function DayListItem(props) {
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !(props.spots)
   });
 
-  const formatSpots = () => {
-    if (!props.spots) {
-      return "no spots remaining";
-    } else if (props.spots === 1) {
-      return "1 spot remaining";
-    } else {
-      return props.spots + " spots remaining";
-    }
-  }
-
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)} data-testid="day">
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{formatSpots()}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
