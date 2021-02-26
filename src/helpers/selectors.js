683aa33758a506eefa63eaff96b94f4426eb74dc
Jinny Jang <jangjinny21@gmail.com>
2021-02-26 01:33:04 -0800

    refractor getAppsForDays and getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,42 +1,29 @@
-export function getAppointmentsForDay (state, day) {
-  const res = [];
-  const filteredDays = state.days.filter(days => days.name === day); // filter returns: [ { id: 1, name: 'Monday', appointments: [ 1, 2, 3 ] } ] find: returns object
-
-  if (!filteredDays.length) {
-    return filteredDays;
+export function getAppointmentsForDay(state, day) {
+  const foundDay = state.days.find((days) => days.name === day);
+  if (!state.days.length || !foundDay) {
+    return [];
   }
 
-  const appsArr = filteredDays[0].appointments; // [ 1, 2, 3 ]
-  appsArr.map(id => res.push(state.appointments[id]));
-  return res;
-
+  return foundDay.appointments.map((id) => state.appointments[id]);
 };
 
-// a function to return a new object containing the interview data when we pass it an object that contains the interviewer -- otherwise null
 export function getInterview (state, interview) {
-  const res = {};
-  const interviewerKey = Object.keys(state.interviewers); // [ 1, 2 ]
-
-  if (!interview) {   // return early if its falsy
+  if (!interview) { 
     return null;
   };
-  
-  interviewerKey.forEach(key => {
-    if (interview.interviewer === parseInt(key)) { // 2 === 2
-      res.student = interview.student;
-      res.interviewer = state.interviewers[key];
-    }
-  });
-  
-  return res;
 
+  const id = interview.interviewer;
+  const newInterview = {...interview, interviewer: state.interviewers[id]}
+
+  return newInterview;
 };
 
 export function getInterviewersForDay(state, day) {
   const foundDay = state.days.find((days) => days.name === day);
-  if (state.days.length === 0 || foundDay === undefined) {
+  if (!state.days.length || !foundDay) {
     return [];
   }
+
   return foundDay.interviewers.map((id) => state.interviewers[id]);
-}
+};
 
